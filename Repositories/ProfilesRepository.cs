454ab00623e15a369bfbd43e1519f1bfe8f92074
ethanvachon <ethanhvachon@gmail.com>
2021-06-25 15:44:16 -0600

    profiles repo

--- Repositories/ProfilesRepository.cs
+++ Repositories/ProfilesRepository.cs
@@ -1,28 +1,52 @@
 using System;
+using System.Data;
+using Dapper;
 using react_back.Models;
 
 namespace react_back.Repositories
 {
   public class ProfilesRepository
   {
+    private readonly IDbConnection _db;
+
+    public ProfilesRepository(IDbConnection db)
+    {
+      _db = db;
+    }
     internal object GetAll()
     {
-      throw new NotImplementedException();
+      string sql = "SELECT * FROM profiles";
+      return _db.Query(sql);
     }
 
     internal Profile GetOne(string id)
     {
-      throw new NotImplementedException();
-    }
-
-    internal object Edit(Profile newProfile)
-    {
-      throw new NotImplementedException();
+      string sql = "SELECT * FROM profiles WHERE id = @id";
+      return _db.QueryFirstOrDefault<Profile>(sql, new { id });
     }
 
     internal object Create(Profile userInfo)
     {
-      throw new NotImplementedException();
+      string sql = @"
+      INSERT INTO profiles
+      (name, picture, email, id)
+      VALUES
+      (@Name, @Picture, @Email, @Id)
+      ;";
+      _db.Execute(sql, userInfo);
+      return userInfo;
     }
+    internal object Edit(Profile newProfile)
+    {
+      string sql = @"
+      UPDATE profiles
+      SET
+      picture = @Picture,
+      name = @Name
+      WHERE id = @id;";
+      _db.Execute(sql, newProfile);
+      return newProfile;
+    }
+
   }
 }
