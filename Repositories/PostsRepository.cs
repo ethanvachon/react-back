88717c307c3707790d160e54694f756e1abfdaa4
ethanvachon <ethanhvachon@gmail.com>
2021-06-25 15:50:49 -0600

    posts repo

--- Repositories/PostsRepository.cs
+++ Repositories/PostsRepository.cs
@@ -1,34 +1,69 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Dapper;
 using react_back.Models;
 
 namespace react_back.Repositories
 {
   public class PostsRepository
   {
+    private readonly IDbConnection _db;
+
+    public PostsRepository(IDbConnection db)
+    {
+      _db = db;
+    }
     internal IEnumerable<Post> GetAll()
     {
-      throw new NotImplementedException();
+      string sql = @"
+      SELECT
+      p.*,
+      pr.*
+      FROM posts p
+      JOIN profiles pr ON p.creatorId = pr.id";
+      return _db.Query<Post, Profile, Post>(sql, (post, profile) => { post.Creator = profile; return post; }, splitOn: "id");
     }
 
     internal Post GetOne(int id)
     {
-      throw new NotImplementedException();
+      string sql = @"
+      SELECT
+      p.*,
+      pr.*
+      FROM posts p
+      JOIN profiles pr ON p.creatorId = pr.id
+      WHERE p.id = @id";
+      return _db.Query<Post, Profile, Post>(sql, (post, profile) => { post.Creator = profile; return post; }, new { id }, splitOn: "id").FirstOrDefault();
     }
 
     internal int Create(Post newPost)
     {
-      throw new NotImplementedException();
+      string sql = @"
+      INSERT INTO questions
+      (time, body, creatorId)
+      VALUES 
+      (@Time, @Body, @CreatorId);
+      SELECT LAST_INSERT_ID();";
+      return _db.ExecuteScalar<int>(sql, newPost);
     }
 
     internal Post Edit(Post newPost)
     {
-      throw new NotImplementedException();
+      string sql = @"
+      UPDATE posts
+      SET
+      body = @Body
+      WHERE id = @id;";
+      _db.Execute(sql, newPost);
+      return newPost;
     }
 
-    internal void Delete(int postId)
+    internal void Delete(int id)
     {
-      throw new NotImplementedException();
+      string sql = "DELETE FROM posts WHERE id = @id LIMIT 1;";
+      _db.Execute(sql, new { id });
     }
   }
 }
